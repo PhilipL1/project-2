6f647ba6a77fde5aa1c1138177c73d5d5afcd8b5
Philip <Philiplartey1@hotmail.com>
2021-06-02 11:56:30 +0000

    jenkinsfile build process

--- jenkins/setup-docker.sh
+++ jenkins/setup-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Install docker 
-curl https://get.docker.com | sudo bash 
+#curl https://get.docker.com | sudo bash 
 
 #add jenkins to docker group 
-sudo usermod -aG docker jenkins 
+#sudo usermod -aG docker jenkins 
 
 #Log in to Docker 
 docker login -u philipl1 $DOCKER_LOGIN_USR -p $DOCKER_LOGIN_PSW
